f5f69fe8056ffd7473f038a15bfc33254c9de6a9
Nguyen Van Dang <nguyendangdh1@gmail.com>
2026-08-28 15:11:53 +0700

    Remove unused model files and clean up imports

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -3,11 +3,3 @@ import models.PoseEstimation
 import models.Multimodel
 import models.Promt_Encoder
 import models.project_net
-# import models.ARTS
-# import models.PoseEstimation
-# import models.SemiAnalytical
-# import models.project_net
-
-# import models.posenet
-# import models.meshnet
-# import models.Graphormer
